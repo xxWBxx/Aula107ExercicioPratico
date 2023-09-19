c36b479b8834416a0d11b2ca2a25a4dca16c8e98
Wallace Breno <wb.coelho@outlook.com>
2023-09-18 22:23:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function person (firts, last, age, eye) {
+    this.firtsName = firts;
+    this.lastName = last;
+    this.age = agr;
+    this.eye = eye;
+}
+
+const myFather = new person ("Wallace", "Coelho", 24, "castanho")
+console.log(myFather)
